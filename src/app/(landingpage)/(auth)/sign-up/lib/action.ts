b3d008bc5af8262ea7c348861cace1778b9b3c90
Rafi <mcintoshhd@Mcintoshs-Air.gateway>
2024-06-09 11:37:02 +1000

    integrate signup with action , making components for form

---
+++ src/app/(landingpage)/(auth)/sign-up/lib/action.ts
@@ -0,0 +1,42 @@
+"use server";
+
+import type { ActionResult } from "@/app/dashboard/(auth)/signin/form/actions";
+import { userSchema } from "./validation";
+import bcrypt from "bcrypt";
+import { redirect } from "next/navigation";
+import prisma from "../../../../../../lib/prisma";
+
+export async function signUpUser(
+  prevState: unknown,
+  formData: FormData
+): Promise<ActionResult> {
+  const validate = userSchema.safeParse({
+    name: formData.get("name"),
+    email: formData.get("email"),
+    passport: formData.get("passport"),
+    password: formData.get("password"),
+  });
+
+  if (!validate.success) {
+    const errorDesc = validate.error.issues.map((issue) => issue.message);
+
+    return {
+      errorTitle: "Error Validation",
+      errorDesc,
+    };
+  }
+
+  const hashingPassword = bcrypt.hashSync(validate.data.password, 10);
+
+  await prisma.user.create({
+    data: {
+      name: validate.data.name,
+      email: validate.data.email,
+      passport: validate.data.passport,
+      password: hashingPassword,
+      role: "CUSTOMER",
+    },
+  });
+
+  return redirect("/sign-in");
+}
