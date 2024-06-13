ce2ddb69264031635db4a9a92b5b663ecb928d63
Rafi <mcintoshhd@Mcintoshs-Air.gateway>
2024-06-13 22:10:28 +1000

    filter by airline, changing api planeIds, search by body

--- src/app/(home)/available-flights/providers/flight-provider.tsx
+++ src/app/(home)/available-flights/providers/flight-provider.tsx
@@ -2,11 +2,56 @@
 
 import type { Airplane, Flight } from "@prisma/client";
 import { useQuery } from "@tanstack/react-query";
-import React, { createContext, type FC, type ReactNode } from "react";
+import React, {
+  Dispatch,
+  createContext,
+  useReducer,
+  type FC,
+  type ReactNode,
+} from "react";
 import axios from "axios";
 
 interface FlightProviderProps {
   children: ReactNode;
+}
+
+export enum FilterActionKind {
+  ADD_PLANE = "ADD_PLANE",
+  REMOVE_PLANE = "REMOVE_PLANE",
+}
+
+type FilterState = {
+  departure?: string | null;
+  arrival?: string | null;
+  date?: string | null;
+  planeId: string;
+  planeIds: string[];
+  seat?: string | null;
+};
+
+type FilterAction = {
+  type: FilterActionKind;
+  payload: Omit<FilterState, "planeIds">;
+};
+
+function filterReducer(state: FilterState, action: FilterAction): FilterState {
+  const { payload, type } = action;
+
+  switch (type) {
+    case FilterActionKind.ADD_PLANE:
+      return {
+        ...state,
+        planeIds: [...state.planeIds, payload.planeId],
+      };
+    case FilterActionKind.REMOVE_PLANE:
+      return {
+        ...state,
+        planeIds: state.planeIds.filter((item) => item !== payload.planeId),
+      };
+
+    default:
+      return state;
+  }
 }
 
 export type FlightWithPlane = Flight & {
@@ -16,18 +61,29 @@ export type FlightWithPlane = Flight & {
 export type FContext = {
   flights: FlightWithPlane[] | undefined;
   isLoading: boolean;
+  dispatch: Dispatch<FilterAction>;
 };
 
 export const FlightContext = createContext<FContext | null>(null);
 
 const FlightProvider: FC<FlightProviderProps> = ({ children }) => {
+  const [state, dispatch] = useReducer(filterReducer, {
+    arrival: null,
+    date: null,
+    departure: null,
+    planeId: "",
+    planeIds: [],
+    seat: null,
+  });
+
   const { data, isLoading } = useQuery({
-    queryKey: ["flights-list"],
-    queryFn: () => axios.get("/api/flights").then((res) => res.data.data),
+    queryKey: ["flights-list", state],
+    queryFn: () =>
+      axios.post("/api/flights", state).then((res) => res.data.data),
   });
 
   return (
-    <FlightContext.Provider value={{ flights: data, isLoading }}>
+    <FlightContext.Provider value={{ flights: data, isLoading, dispatch }}>
       {children}
     </FlightContext.Provider>
   );
--- src/app/api/flights/route.ts
+++ src/app/api/flights/route.ts
@@ -2,32 +2,24 @@ import type { NextRequest } from "next/server";
 import prisma from "../../../../lib/prisma";
 import type { TypeSeat } from "@prisma/client";
 
-export async function GET(request: NextRequest) {
-  const searchParams = request.nextUrl.searchParams;
-
-  const params = {
-    departure: searchParams.get("departure"),
-    arrival: searchParams.get("arrival"),
-    date: searchParams.get("date"),
-    planeId: searchParams.get("planeId"),
-    seat: searchParams.get("seat"),
-  };
+export async function POST(request: NextRequest) {
+  const body = await request.json();
 
   let departureDate: Date | null = null;
 
-  if (params.date) {
-    departureDate = new Date(params.date);
+  if (body.date) {
+    departureDate = new Date(body.date);
     departureDate.setHours(1);
   }
 
   try {
     const data = await prisma.flight.findMany({
       where: {
-        departureCity: params.departure !== null ? params.departure : {},
-        destinationCity: params.arrival !== null ? params.arrival : {},
+        departureCity: body.departure !== null ? body.departure : {},
+        destinationCity: body.arrival !== null ? body.arrival : {},
         seats:
-          params.seat !== null
-            ? { some: { type: params.seat as TypeSeat, isBooked: false } }
+          body.seat !== null
+            ? { some: { type: body.seat as TypeSeat, isBooked: false } }
             : {},
         departureDate:
           departureDate !== null
@@ -36,12 +28,10 @@ export async function GET(request: NextRequest) {
               }
             : {},
         planeId:
-          params.planeId !== null
-            ? params.planeId.split(",").length > 0
-              ? {
-                  in: [...params.planeId.split(",")],
-                }
-              : {}
+          body.planeIds.length > 0
+            ? {
+                in: body.planeIds,
+              }
             : {},
       },
       include: {
